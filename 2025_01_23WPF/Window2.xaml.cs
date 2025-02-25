30755e0d06e889e9e177a536fd7b7cd06aef029b
Rendszergazda <rendszergazda@blathy.info>
2025-02-25 14:51:42 +0100

    made changes to window 2

--- 2025_01_23WPF/Window2.xaml.cs
+++ 2025_01_23WPF/Window2.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Intrinsics.Arm;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -19,9 +21,47 @@ namespace _2025_01_23WPF
     /// </summary>
     public partial class Window2 : Window
     {
+        private const string FilePath = "todo.csv";
+        private List<TodoItem> toDoItems = new List<TodoItem>();
         public Window2()
         {
             InitializeComponent();
+        }
+
+        private void SaveItem()
+        {
+            var operation = new List<string>
+            {
+                //TBInput.Text.ToCsv()
+            };
+            File.WriteAllLines(FilePath, operation);
+        }
+
+        private void AddBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if(TBInput.Text != null)
+            {
+                var operation = new List<string>();
+                var item = new TodoItem
+                {
+                    Value = TBInput.Text,
+                    Deadline = DeadLineDP.SelectedDate,
+                    CompletedAt = CB.IsChecked == true ? DateTime.Now : null
+                };
+                TBInput.Clear();
+                CB.IsChecked = false;
+                DeadLineDP.SelectedDate = null;
+                SaveItem();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void ReturnBTN_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();   
         }
     }
 }
